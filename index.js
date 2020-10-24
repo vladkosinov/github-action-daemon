7953de89baaaa8064e9336155482c34a1f636a42
Vlad Kosinov <4584879+vladkosinov@users.noreply.github.com>
2020-10-24 17:06:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.on("text", async function (ctx) {
   const { text } = ctx.message;
-  ctx.reply("You said: " + text);
+  ctx.reply("You said this: " + text);
 });
 
 bot.launch();
